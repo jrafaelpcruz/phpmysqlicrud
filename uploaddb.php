2537347f5f18557aa8eb93c1f3777954000e612f
João Rafael P Cruz <100492774+jrafaelpcruz@users.noreply.github.com>
2022-06-07 13:23:43 -0300

    Delete uploaddb.php

--- uploaddb.php
+++
@@ -1,7 +0,0 @@
-<?php
-  define('HOST', 'localhost');
-  define('USER', 'root');
-  define('PASS', 'root');
-  define('BASE', 'itsbob');
-$conn = new MysQLi(HOST, USER,PASS,BASE) or die("erro ".mysqli_error);
-?>
