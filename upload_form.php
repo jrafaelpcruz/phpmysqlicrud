a1ca42a8113b971c2407b530a7afb30f0f943ac4
João Rafael P Cruz <100492774+jrafaelpcruz@users.noreply.github.com>
2022-06-07 13:23:36 -0300

    Delete upload_form.php

--- upload_form.php
+++
@@ -1,9 +0,0 @@
-<form action="upload.php" method="POST" enctype="multipart/form-data">
-<div class="mb-3">
-    <label>Foto</label>
-    <input type="file" name="foto" class="form-control"/>
-  </div>
-  <div class="mb-3">
-    <button type="submit" class="btn-primary">Enviar</button>
-  </div>
-</form>
